4a9e61785fd79d8bd08bdfbd87c35588bda0f5d7
Rodrigo Roa <rodrigo.roa@whiz.one>
2022-08-23 16:40:39 -0500

    fix: update payment routes

--- src/api/payment/payment.route.js
+++ src/api/payment/payment.route.js
@@ -7,5 +7,9 @@ const controller = new Controller();
 
 router.get('/:operatorId', catchError(controller.list));
 router.get('/patient', catchError(controller.listByPatient));
+router.post('/register', catchError(controller.register));
+router.put('/update', catchError(controller.update));
+router.post('/remove', catchError(controller.delete));
+router.post('/send-payment-link', catchError(controller.sendPaymentLink));
 
 module.exports = router;
